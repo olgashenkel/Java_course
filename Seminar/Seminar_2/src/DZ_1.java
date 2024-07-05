ce5bd3e8b57a34c575de2ef91f2a0cfef519bb14
olga_shenkel <olga_shenkel@mail.ru>
2024-07-05 02:03:59 +0200

    add Java_course/Seminar_2

--- Seminar/Seminar_2/src/DZ_1.java
+++ Seminar/Seminar_2/src/DZ_1.java
@@ -1,26 +1,73 @@
-public class DZ_1 {
+// Сформируйте SQL-запрос
+//
+//Дана строка sql-запроса:
+//select * from students where "
+//Сформируйте часть WHERE этого запроса, используя StringBuilder. Данные (параметры) для фильтрации приведены в виде json-строки в примере ниже. Если значение null, то параметр не должен попадать в запрос.
+//
+//Напишите свой код в методе answer класса Answer. Метод answer принимает на вход два параметра:
+//String QUERY - начало SQL-запроса String PARAMS - JSON с параметрами
+//
+//Пример: Строка sql-запроса:
+//select * from students where
+//
+//Параметры для фильтрации:
+// {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+//
+//Результат:
+//select * from students where name='Ivanov' and country='Russia' and city='Moscow'
 
+class Answer {
+    public static StringBuilder answer(String QUERY, String PARAMS){
+        // Напишите свое решение ниже
+        PARAMS = PARAMS.replace("{", "").replace("}", "").replace(":", ",").replace(" ", "").replaceAll("\"", "");
+        String[] jsonArray = PARAMS.split(",");
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(QUERY);
+
+        boolean firstCondition = true;
+
+        for (int i = 0; i < jsonArray.length; i += 2) {
+            String key = jsonArray[i];
+            String value = jsonArray[i + 1];
+
+            if (!value.equals("null")) {
+                if (!firstCondition) {
+                    sb.append(" and ");
+                }
+                sb.append(key).append("='").append(value).append("'");
+                firstCondition = false;
+            }
+        }
+
+        return sb;
+    }
 }
+
+
+// Не удаляйте этот класс - он нужен для вывода результатов на экран и проверки
+public class DZ_1{
+    public static void main(String[] args) {
+        String QUERY = "";
+        String PARAMS = "";
+
+        if (args.length == 0) {
+            // При отправке кода на Выполнение, вы можете варьировать эти параметры
+            QUERY = "select * from students where ";
+            PARAMS = "{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"} ";
+        }
+        else{
+            QUERY = args[0];
+            PARAMS = args[1];
+        }
+
+        Answer ans = new Answer();
+        System.out.println(ans.answer(QUERY, PARAMS));
+    }
+}
+
+
 /*
-Сформируйте SQL-запрос
-
-Дана строка sql-запроса:
-select * from students where "
-Сформируйте часть WHERE этого запроса, используя StringBuilder. Данные (параметры) для фильтрации приведены в виде json-строки в примере ниже. Если значение null, то параметр не должен попадать в запрос.
-
-Напишите свой код в методе answer класса Answer. Метод answer принимает на вход два параметра:
-String QUERY - начало SQL-запроса String PARAMS - JSON с параметрами
-
-Пример: Строка sql-запроса:
-select * from students where
-
-Параметры для фильтрации:
- {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
-
-Результат:
-select * from students where name='Ivanov' and country='Russia' and city='Moscow'
-
-
 //*****************************
 Решение с сайта:
 
